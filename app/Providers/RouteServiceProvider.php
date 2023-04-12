cc2d1e6dcef97163743c0654f9236bcd6a5da3e1
songke1313 <songke1313@gmail.com>
2023-04-12 21:39:07 +0900

    基础页面

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -13,11 +13,13 @@ class RouteServiceProvider extends ServiceProvider
     /**
      * The path to the "home" route for your application.
      *
-     * Typically, users are redirected here after authentication.
+    * Typically, users are redirected here after authentication.
      *
      * @var string
      */
     public const HOME = '/home';
+
+    protected  $namespace =  'App\\Http\\Controllers';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
@@ -34,6 +36,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
     }
